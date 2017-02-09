43f59bf482105a6acc765a18b03d2a833700886d
Pinterf <Pinterf@gmail.com>
2017-02-09 19:18:58 +0100

    Pixel bytesize to Plane for proper offset calculation

--- common/plane/plane.h
+++ common/plane/plane.h
@@ -14,13 +14,14 @@ protected:
    ptrdiff_t nPitch;
    int nWidth;
    int nHeight;
+   int nPixelSize; // 1 for 8 bit, 2 for 10-16 bit, 4 for float
 
 public:
 
-   Plane() : pPixel(NULL), nPitch(0), nWidth(0), nHeight(0) {}
-   Plane(T *pPixel, ptrdiff_t nPitch, int nWidth, int nHeight) : pPixel(pPixel), nPitch(nPitch), nWidth(nWidth), nHeight(nHeight) { }
+   Plane() : pPixel(NULL), nPitch(0), nWidth(0), nHeight(0), nPixelSize(1) {}
+   Plane(T *pPixel, ptrdiff_t nPitch, int nWidth, int nHeight, int nPixelSize) : pPixel(pPixel), nPitch(nPitch), nWidth(nWidth), nHeight(nHeight), nPixelSize(nPixelSize) { }
 
-   Plane(const Plane<const T> &plane) : pPixel(plane.data()), nPitch(plane.pitch()), nWidth(plane.width()), nHeight(plane.height())
+   Plane(const Plane<const T> &plane) : pPixel(plane.data()), nPitch(plane.pitch()), nWidth(plane.width()), nHeight(plane.height()), nPixelSize(plane.pixelsize())
    {
    }
 
@@ -28,8 +29,12 @@ public:
    ptrdiff_t pitch() const { return nPitch; }
    int width() const { return nWidth; }
    int height() const { return nHeight; }
+   int pixelsize() const { return nPixelSize; }
 
-   Plane<T> offset(int x = 0, int y = 0, int w = 0, int h = 0) const { return Plane<T>( pPixel + x + y * nPitch, nPitch, w, h ); }
+   Plane<T> offset(int x = 0, int y = 0, int w = 0, int h = 0) const 
+   { 
+     return Plane<T>( pPixel + x * nPixelSize + y * nPitch, nPitch, w, h, nPixelSize ); // byte pointers->x*pixelsize
+   }
    void print() const { Filtering::print( LOG_DEBUG, "plane 0x%x (%ix%i:%i) t%i\n", pPixel, nWidth, nHeight, nPitch, GetCurrentThreadId() ); }
 
 };
