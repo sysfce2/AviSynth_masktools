b91743150fe347953003d0cc760a478e13835a6a
Pinterf <Pinterf@gmail.com>
2017-02-11 20:52:25 +0100

    mt_adddiff: 10-16 bits

--- masktools/filters/support/adddiff16/adddiff16.cpp
+++ masktools/filters/support/adddiff16/adddiff16.cpp
@@ -4,29 +4,40 @@
 
 namespace Filtering { namespace MaskTools { namespace Filters { namespace Support  { namespace AddDiff16 {
 
+template<int bits_per_pixel>
 static MT_FORCEINLINE Word adddiff16_core_c(Word dst, Word src) {
-    return clip<Word, int>(int(dst) + src - (65536 / 2), 0, 65535);
+    const int max_pixel_value = (1 << bits_per_pixel) - 1;
+    const int half = 1 << (bits_per_pixel - 1);
+    return clip<Word, int>(int(dst) + src - (half), 0, max_pixel_value);
 }
 
-static MT_FORCEINLINE __m128i adddiff16_core_sse2(const __m128i &dst, const __m128i &src, const __m128i &halfrange) {
+template<bool lessThan16bits>
+static MT_FORCEINLINE __m128i adddiff16_core_sse2(const __m128i &dst, const __m128i &src, const __m128i &halfrange, const __m128i &max_pixel_value) {
+  if (lessThan16bits) {
+    auto val = _mm_add_epi16(src, dst);
+    auto result = _mm_subs_epu16(val, halfrange);
+    return _mm_min_epi16(result, max_pixel_value);
+  }
+  else {
     auto dstval = _mm_sub_epi16(dst, halfrange);
     auto srcval = _mm_sub_epi16(src, halfrange);
     auto result = _mm_adds_epi16(dstval, srcval);
     return _mm_add_epi16(result, halfrange);
+  }
 }
 
 
-void adddiff16_stacked_c(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nWidth, int nHeight)
+void adddiff16_stacked_c(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nWidth, int nHeight, int nOrigHeight)
 {
-   auto pDstLsb = pDst + nDstPitch * nHeight / 2;
-   auto pSrcLsb = pSrc + nSrcPitch * nHeight / 2;
+   auto pDstLsb = pDst + nDstPitch * nOrigHeight / 2;
+   auto pSrcLsb = pSrc + nSrcPitch * nOrigHeight / 2;
 
    for (int y = 0; y < nHeight / 2; y++) {
        for (int x = 0; x < nWidth; x++) {
            Word dst = read_word_stacked(pDst, pDstLsb, x);
            Word src = read_word_stacked(pSrc, pSrcLsb, x);
 
-           Word result = adddiff16_core_c(dst, src);
+           Word result = adddiff16_core_c<16>(dst, src);
 
            write_word_stacked(pDst, pDstLsb, x, result);
        }
@@ -37,14 +48,15 @@ void adddiff16_stacked_c(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrd
    }
 }
 
-void adddiff16_interleaved_c(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nWidth, int nHeight)
+template<int bits_per_pixel>
+void adddiff16_native_c(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nWidth, int nHeight, int)
 {
     for (int y = 0; y < nHeight; y++) {
         auto pDstWord = reinterpret_cast<Word*>(pDst);
         auto pSrcWord = reinterpret_cast<const Word*>(pSrc);
 
-        for (int x = 0; x < nWidth / 2; x++) {
-            pDstWord[x] = adddiff16_core_c(pDstWord[x], pSrcWord[x]);
+        for (int x = 0; x < nWidth; x++) { // width is real width
+          pDstWord[x] = adddiff16_core_c<bits_per_pixel>(pDstWord[x], pSrcWord[x]);
         }
         pDst += nDstPitch;
         pSrc += nSrcPitch;
@@ -53,14 +65,14 @@ void adddiff16_interleaved_c(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, 
 
 #pragma warning(disable: 4309)
 
-void adddiff16_stacked_sse2(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nWidth, int nHeight)
+void adddiff16_stacked_sse2(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nWidth, int nHeight, int nOrigHeight)
 {
     int wMod8 = (nWidth / 8) * 8;
     auto pDst2 = pDst;
     auto pSrc2 = pSrc;
 
-    auto pDstLsb = pDst + nDstPitch * nHeight / 2;
-    auto pSrcLsb = pSrc + nSrcPitch * nHeight / 2;
+    auto pDstLsb = pDst + nDstPitch * nOrigHeight / 2;
+    auto pSrcLsb = pSrc + nSrcPitch * nOrigHeight / 2;
 
     auto ff = _mm_set1_epi16(0x00FF);
     auto zero = _mm_setzero_si128();
@@ -70,8 +82,7 @@ void adddiff16_stacked_sse2(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, p
         for ( int i = 0; i < wMod8; i+=8 ) {
             auto dst = read_word_stacked_simd(pDst, pDstLsb, i);
             auto src = read_word_stacked_simd(pSrc, pSrcLsb, i);
-
-            auto result = adddiff16_core_sse2(dst, src, halfrange);
+            auto result = adddiff16_core_sse2<false>(dst, src, halfrange, halfrange /*dummy*/);
 
             write_word_stacked_simd(pDst, pDstLsb, i, result, ff, zero);
         }
@@ -82,24 +93,33 @@ void adddiff16_stacked_sse2(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, p
     }
 
     if (nWidth > wMod8) {
-        adddiff16_stacked_c(pDst2 + wMod8, nDstPitch, pSrc2 + wMod8, nSrcPitch, nWidth - wMod8, nHeight);
+        adddiff16_stacked_c(pDst2 + wMod8, nDstPitch, pSrc2 + wMod8, nSrcPitch, nWidth - wMod8, nHeight, nOrigHeight);
     }
 }
 
-void adddiff16_interleaved_sse2(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nWidth, int nHeight)
+template<int bits_per_pixel>
+void adddiff16_native_sse2(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nWidth, int nHeight, int nOrigHeight)
 {
+    nWidth *= 2; // really rowsize: width * sizeof(uint16), see also division at C trailer
+
     int wMod16 = (nWidth / 16) * 16;
 
     auto pDst2 = pDst;
     auto pSrc2 = pSrc;
-    auto halfrange = _mm_set1_epi16(0x8000);
+    auto halfrange = _mm_set1_epi16(1 << (bits_per_pixel - 1));
+    int _max_pixel_value = (1 << bits_per_pixel) - 1;
+    auto max_pixel_value = _mm_set1_epi16(_max_pixel_value);
 
     for (int j = 0; j < nHeight; ++j) {
         for (int i = 0; i < wMod16; i+=16) {
             auto dst = simd_load_si128<MemoryMode::SSE2_UNALIGNED>(reinterpret_cast<const __m128i*>(pDst+i));
             auto src = simd_load_si128<MemoryMode::SSE2_UNALIGNED>(reinterpret_cast<const __m128i*>(pSrc+i));
 
-            auto result = adddiff16_core_sse2(dst, src, halfrange);
+            __m128i result;
+            if(bits_per_pixel < 16)
+              result = adddiff16_core_sse2<true>(dst, src, halfrange, max_pixel_value);
+            else
+              result = adddiff16_core_sse2<false>(dst, src, halfrange, max_pixel_value);
 
             simd_store_si128<MemoryMode::SSE2_UNALIGNED>(reinterpret_cast<__m128i*>(pDst+i), result);
         }
@@ -107,9 +127,27 @@ void adddiff16_interleaved_sse2(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSr
         pSrc += nSrcPitch;
     }
     if (nWidth > wMod16) {
-        adddiff16_interleaved_c(pDst2 + wMod16, nDstPitch, pSrc2 + wMod16, nSrcPitch, nWidth - wMod16, nHeight);
+        adddiff16_native_c<bits_per_pixel>(pDst2 + wMod16, nDstPitch, pSrc2 + wMod16, nSrcPitch, (nWidth - wMod16)/sizeof(uint16_t), nHeight, nOrigHeight);
     }
 }
 
+#define MAKE_TEMPLATES(bits_per_pixel) \
+template void adddiff16_native_sse2<##bits_per_pixel##>(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nWidth, int nHeight, int nOrigHeight); \
+template void adddiff16_native_c<##bits_per_pixel##>(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nWidth, int nHeight, int nOrigHeight);
+MAKE_TEMPLATES(10)
+MAKE_TEMPLATES(12)
+MAKE_TEMPLATES(14)
+MAKE_TEMPLATES(16)
+#undef MAKE_TEMPLATES
+
+#define MAKE_EXPORTS(bits_per_pixel) \
+Processor *adddiff16_native_##bits_per_pixel##_c = &adddiff16_native_c<##bits_per_pixel##>; \
+Processor *adddiff16_native_##bits_per_pixel##_sse2 = &adddiff16_native_sse2<##bits_per_pixel##>; 
+
+MAKE_EXPORTS(10)
+MAKE_EXPORTS(12)
+MAKE_EXPORTS(14)
+MAKE_EXPORTS(16)
+#undef MAKE_EXPORTS
 
 } } } } }
--- masktools/filters/support/adddiff16/adddiff16.h
+++ masktools/filters/support/adddiff16/adddiff16.h
@@ -5,13 +5,19 @@
 
 namespace Filtering { namespace MaskTools { namespace Filters { namespace Support { namespace AddDiff16 {
 
-typedef void(Processor)(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nWidth, int nHeight);
+typedef void(Processor)(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nWidth, int nHeight, int nOrigHeight);
 
 Processor adddiff16_stacked_c;
 Processor adddiff16_stacked_sse2;
 
-Processor adddiff16_interleaved_c;
-Processor adddiff16_interleaved_sse2;
+extern Processor *adddiff16_native_10_c;
+extern Processor *adddiff16_native_12_c;
+extern Processor *adddiff16_native_14_c;
+extern Processor *adddiff16_native_16_c;
+extern Processor *adddiff16_native_10_sse2;
+extern Processor *adddiff16_native_12_sse2;
+extern Processor *adddiff16_native_14_sse2;
+extern Processor *adddiff16_native_16_sse2;
 
 class AddDiff16 : public MaskTools::Filter
 {
@@ -23,25 +29,52 @@ protected:
    virtual void process(int n, const Plane<Byte> &dst, int nPlane, const Filtering::Frame<const Byte> frames[3], const Constraint constraints[3]) override
    {
       UNUSED(n);
-      processors.best_processor( constraints[nPlane] )( dst.data(), dst.pitch(), frames[0].plane(nPlane).data(), frames[0].plane(nPlane).pitch(), dst.width(), dst.height() );
+      processors.best_processor( constraints[nPlane] )( dst.data(), dst.pitch(), frames[0].plane(nPlane).data(), frames[0].plane(nPlane).pitch(), dst.width(), dst.height(), dst.origheight() );
    }
 
 public:
     AddDiff16(const Parameters &parameters) : MaskTools::Filter( parameters, FilterProcessingType::INPLACE )
     {
-        /* add the processors */
+      bool isStacked = parameters["stacked"].toBool();
+      int bits_per_pixel = bit_depths[C];
+
+      if (isStacked && bits_per_pixel != 8) {
+        error = "Stacked specified for a non-8 bit clip";
+        return;
+      }
+      if (!isStacked && bits_per_pixel == 8) {
+        error = "8 bit clip needs stacked=true";
+        return;
+      }
+      if (bits_per_pixel == 32) {
+        error = "32 bit float clip is not supported yet";
+        return;
+      }
+      
+      /* add the processors */
         if (parameters["stacked"].toBool() == true) {
-            processors.push_back(Filtering::Processor<Processor>(adddiff16_stacked_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0));
-            processors.push_back(Filtering::Processor<Processor>(adddiff16_stacked_sse2, Constraint(CPU_SSE2, 1, 1, 1, 1), 1));
+            processors.push_back(Filtering::Processor<Processor>(adddiff16_stacked_c, Constraint(CPU_NONE, MODULO_NONE, MODULO_NONE, ALIGNMENT_NONE, 1), 0));
+            processors.push_back(Filtering::Processor<Processor>(adddiff16_stacked_sse2, Constraint(CPU_SSE2, MODULO_NONE, MODULO_NONE, ALIGNMENT_NONE, 1), 1));
         } else {
-            processors.push_back(Filtering::Processor<Processor>(adddiff16_interleaved_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0));
-            processors.push_back(Filtering::Processor<Processor>(adddiff16_interleaved_sse2, Constraint(CPU_SSE2, 1, 1, 1, 1), 1));
+          switch (bit_depths[C]) {
+          case 10: 
+            processors.push_back(Filtering::Processor<Processor>(adddiff16_native_10_c, Constraint(CPU_NONE, MODULO_NONE, MODULO_NONE, ALIGNMENT_NONE, 1), 0));
+            processors.push_back(Filtering::Processor<Processor>(adddiff16_native_10_sse2, Constraint(CPU_SSE2, MODULO_NONE, MODULO_NONE, ALIGNMENT_NONE, 1), 1));
+            break;
+          case 12:
+            processors.push_back(Filtering::Processor<Processor>(adddiff16_native_12_c, Constraint(CPU_NONE, MODULO_NONE, MODULO_NONE, ALIGNMENT_NONE, 1), 0));
+            processors.push_back(Filtering::Processor<Processor>(adddiff16_native_12_sse2, Constraint(CPU_SSE2, MODULO_NONE, MODULO_NONE, ALIGNMENT_NONE, 1), 1));
+            break;
+          case 14:
+            processors.push_back(Filtering::Processor<Processor>(adddiff16_native_14_c, Constraint(CPU_NONE, MODULO_NONE, MODULO_NONE, ALIGNMENT_NONE, 1), 0));
+            processors.push_back(Filtering::Processor<Processor>(adddiff16_native_14_sse2, Constraint(CPU_SSE2, MODULO_NONE, MODULO_NONE, ALIGNMENT_NONE, 1), 1));
+            break;
+          case 16:
+            processors.push_back(Filtering::Processor<Processor>(adddiff16_native_16_c, Constraint(CPU_NONE, MODULO_NONE, MODULO_NONE, ALIGNMENT_NONE, 1), 0));
+            processors.push_back(Filtering::Processor<Processor>(adddiff16_native_16_sse2, Constraint(CPU_SSE2, MODULO_NONE, MODULO_NONE, ALIGNMENT_NONE, 1), 1));
+            break;
+          }
         }
-        /* sample from 8 bit addiff
-        processors.push_back(Filtering::Processor<Processor>(&adddiff_c, Constraint(CPU_NONE, MODULO_NONE, MODULO_NONE, ALIGNMENT_NONE, 1), 0));
-        processors.push_back(Filtering::Processor<Processor>(adddiff_sse2, Constraint(CPU_SSE2, MODULO_NONE, MODULO_NONE, ALIGNMENT_NONE, 1), 1));
-        processors.push_back(Filtering::Processor<Processor>(adddiff_asse2, Constraint(CPU_SSE2, MODULO_NONE, MODULO_NONE, ALIGNMENT_16, 16), 2));
-        */
     }
 
    InputConfiguration &input_configuration() const { return InPlaceTwoFrame(); }
