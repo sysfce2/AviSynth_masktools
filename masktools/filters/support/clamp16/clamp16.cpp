2e2a4ff5d6d37b3942b3b85df6bc75968f1dc6ca
Pinterf <Pinterf@gmail.com>
2017-02-11 23:11:25 +0100

    mt_clamp16: 10-16 bits

--- masktools/filters/support/clamp16/clamp16.cpp
+++ masktools/filters/support/clamp16/clamp16.cpp
@@ -4,24 +4,31 @@
 
 namespace Filtering { namespace MaskTools { namespace Filters { namespace Support  { namespace Clamp16 {
 
+template<int bits_per_pixel>
 static Word MT_FORCEINLINE clamp16_core_c(Word dst, Word upperLimit, Word lowerLimit, int overshoot, int undershoot) {
     Word result = static_cast<Word>(dst > upperLimit + overshoot ? upperLimit + overshoot : dst);
-    return static_cast<Word>(result < lowerLimit - undershoot ? lowerLimit - undershoot : result);
+    if (bits_per_pixel < 16)
+      return min(static_cast<Word>(result < lowerLimit - undershoot ? lowerLimit - undershoot : result), Word((1 << bits_per_pixel) - 1));
+    else
+      return static_cast<Word>(result < lowerLimit - undershoot ? lowerLimit - undershoot : result);
 }
 
-static __m128i MT_FORCEINLINE clamp16_core_sse2(const __m128i &dst, const __m128i &upperLimit, const __m128i &lowerLimit, const __m128i &overshoot, const __m128i &undershoot) {
+template<CpuFlags flags, bool lessThan16bits>
+static __m128i MT_FORCEINLINE clamp16_core_simd(const __m128i &dst, const __m128i &upperLimit, const __m128i &lowerLimit, const __m128i &overshoot, const __m128i &undershoot, const __m128i &max_pixel_value) {
     auto upper_limit_real = _mm_adds_epu16(upperLimit, overshoot);
+    if (lessThan16bits)
+      upper_limit_real = simd_min_epu16<flags>(upper_limit_real, max_pixel_value);
     auto lower_limit_real = _mm_subs_epu16(lowerLimit, undershoot);
 
-    auto limited = _mm_min_epu16(dst, upper_limit_real);
-    return _mm_max_epu16(limited, lower_limit_real);
+    auto limited = simd_min_epu16<flags>(dst, upper_limit_real); // _mm_min_epu16: SSE4.1 function. Simulated on SSE2
+    return simd_max_epu16<flags>(limited, lower_limit_real); // _mm_max_epu16: SSE4.1 function. Simulated on SSE2
 }
 
-void clamp16_stacked_c(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOvershoot, int nUndershoot)
+void clamp16_stacked_c(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOrigHeight, int nOvershoot, int nUndershoot)
 {
-   auto pDstLsb = pDst + nDstPitch * nHeight / 2;
-   auto pUpLimitLsb = pUpLimit + nUpLimitPitch * nHeight / 2;
-   auto pLowLimitLsb = pLowLimit + nLowLimitPitch * nHeight / 2;
+   auto pDstLsb = pDst + nDstPitch * nOrigHeight / 2;
+   auto pUpLimitLsb = pUpLimit + nUpLimitPitch * nOrigHeight / 2;
+   auto pLowLimitLsb = pLowLimit + nLowLimitPitch * nOrigHeight / 2;
 
    for (int y = 0; y < nHeight / 2; y++) {
        for (int x = 0; x < nWidth; x++) {
@@ -29,7 +36,7 @@ void clamp16_stacked_c(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, pt
            Word upperLimit = read_word_stacked(pUpLimit, pUpLimitLsb, x);
            Word lowerLimit = read_word_stacked(pLowLimit, pLowLimitLsb, x);
 
-           Word result = clamp16_core_c(dst, upperLimit, lowerLimit, nOvershoot, nUndershoot);
+           Word result = clamp16_core_c<16>(dst, upperLimit, lowerLimit, nOvershoot, nUndershoot);
 
            write_word_stacked(pDst, pDstLsb, x, result);
        }
@@ -42,15 +49,16 @@ void clamp16_stacked_c(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, pt
    }
 }
 
-void clamp16_interleaved_c(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOvershoot, int nUndershoot)
+template<int bits_per_pixel>
+void clamp16_native_c(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOrigHeight, int nOvershoot, int nUndershoot)
 {
     for (int y = 0; y < nHeight; y++) {
         auto pDstWord = reinterpret_cast<Word*>(pDst);
         auto pUpLimitWord = reinterpret_cast<const Word*>(pUpLimit);
         auto pLowLimitWord = reinterpret_cast<const Word*>(pLowLimit);
 
-        for (int x = 0; x < nWidth / 2; x++) {
-            pDstWord[x] = clamp16_core_c(pDstWord[x], pUpLimitWord[x], pLowLimitWord[x], nOvershoot, nUndershoot);
+        for (int x = 0; x < nWidth; x++) {
+            pDstWord[x] = clamp16_core_c<bits_per_pixel>(pDstWord[x], pUpLimitWord[x], pLowLimitWord[x], nOvershoot, nUndershoot);
         }
         pDst += nDstPitch;
         pUpLimit += nUpLimitPitch;
@@ -58,21 +66,23 @@ void clamp16_interleaved_c(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit
     }
 }
 
-void clamp16_stacked_sse2(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOvershoot, int nUndershoot)
+template<CpuFlags flags>
+void clamp16_stacked_simd(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOrigHeight, int nOvershoot, int nUndershoot)
 {
     int wMod8 = (nWidth / 8) * 8;
     auto pDst2 = pDst;
     auto pUpLimit2 = pUpLimit;
     auto pLowLimit2 = pLowLimit;
 
-    auto pDstLsb = pDst + nDstPitch * nHeight / 2;
-    auto pUpLimitLsb = pUpLimit + nUpLimitPitch * nHeight / 2;
-    auto pLowLimitLsb = pLowLimit + nLowLimitPitch * nHeight / 2;
+    auto pDstLsb = pDst + nDstPitch * nOrigHeight / 2;
+    auto pUpLimitLsb = pUpLimit + nUpLimitPitch * nOrigHeight / 2;
+    auto pLowLimitLsb = pLowLimit + nLowLimitPitch * nOrigHeight / 2;
 
     auto overshoot_v = _mm_set1_epi16(Word(nOvershoot));
     auto undershoot_v = _mm_set1_epi16(Word(nUndershoot));
     auto ff = _mm_set1_epi16(0x00FF);
     auto zero = _mm_setzero_si128();
+    auto max_pixel_value = _mm_set1_epi16(0xFFFF); // n/a for 16 bits
 
     for ( int j = 0; j < nHeight / 2; ++j ) {
         for ( int i = 0; i < wMod8; i+=8 ) {
@@ -80,7 +90,7 @@ void clamp16_stacked_sse2(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit,
             auto lower_limit = read_word_stacked_simd(pLowLimit, pLowLimitLsb, i);
             auto limited = read_word_stacked_simd(pDst, pDstLsb, i);
 
-            limited = clamp16_core_sse2(limited, upper_limit, lower_limit, overshoot_v, undershoot_v);
+            limited = clamp16_core_simd<flags, false>(limited, upper_limit, lower_limit, overshoot_v, undershoot_v, max_pixel_value);
 
             write_word_stacked_simd(pDst, pDstLsb, i, limited, ff, zero);
         }
@@ -93,12 +103,15 @@ void clamp16_stacked_sse2(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit,
     }
 
     if (nWidth > wMod8) {
-        clamp16_stacked_c(pDst2 + wMod8, nDstPitch, pUpLimit2 + wMod8, nUpLimitPitch, pLowLimit2+wMod8, nLowLimitPitch, nWidth - wMod8, nHeight, nOvershoot, nUndershoot);
+        clamp16_stacked_c(pDst2 + wMod8, nDstPitch, pUpLimit2 + wMod8, nUpLimitPitch, pLowLimit2+wMod8, nLowLimitPitch, nWidth - wMod8, nHeight, nOrigHeight, nOvershoot, nUndershoot);
     }
 }
 
-void clamp16_interleaved_sse2(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOvershoot, int nUndershoot)
+template<CpuFlags flags, int bits_per_pixel>
+void clamp16_native_simd(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOrigHeight, int nOvershoot, int nUndershoot)
 {
+    nWidth *= 2; // really rowsize: width * sizeof(uint16), see also division at C trailer
+  
     int wMod16 = (nWidth / 16) * 16;
     auto pDst2 = pDst;
     auto pUpLimit2 = pUpLimit;
@@ -106,14 +119,18 @@ void clamp16_interleaved_sse2(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLi
 
     auto overshoot_v = _mm_set1_epi16(Word(nOvershoot));
     auto undershoot_v = _mm_set1_epi16(Word(nUndershoot));
+    int _max_pixel_value = (1 << bits_per_pixel) - 1;
+    auto max_pixel_value = _mm_set1_epi16(Word(_max_pixel_value));
 
     for ( int j = 0; j < nHeight; ++j ) {
         for ( int i = 0; i < wMod16; i+=16 ) {
             auto upper_limit = simd_load_si128<MemoryMode::SSE2_UNALIGNED>(reinterpret_cast<const __m128i*>(pUpLimit+i));
             auto lower_limit = simd_load_si128<MemoryMode::SSE2_UNALIGNED>(reinterpret_cast<const __m128i*>(pLowLimit+i));
             auto limited = simd_load_si128<MemoryMode::SSE2_UNALIGNED>(reinterpret_cast<const __m128i*>(pDst+i));
-
-            limited = clamp16_core_sse2(limited, upper_limit, lower_limit, overshoot_v, undershoot_v);
+            if(bits_per_pixel < 16)
+              limited = clamp16_core_simd<flags, true>(limited, upper_limit, lower_limit, overshoot_v, undershoot_v, max_pixel_value);
+            else
+              limited = clamp16_core_simd<flags, false>(limited, upper_limit, lower_limit, overshoot_v, undershoot_v, max_pixel_value);
 
             simd_store_si128<MemoryMode::SSE2_UNALIGNED>(reinterpret_cast<__m128i*>(pDst+i), limited);
         }
@@ -123,9 +140,44 @@ void clamp16_interleaved_sse2(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLi
     }
 
     if (nWidth > wMod16) {
-        clamp16_interleaved_c(pDst2 + wMod16, nDstPitch, pUpLimit2 + wMod16, nUpLimitPitch, pLowLimit2+wMod16, nLowLimitPitch, nWidth - wMod16, nHeight, nOvershoot, nUndershoot);
+        clamp16_native_c<bits_per_pixel>(pDst2 + wMod16, nDstPitch, pUpLimit2 + wMod16, nUpLimitPitch, pLowLimit2+wMod16, nLowLimitPitch, (nWidth - wMod16) / sizeof(uint16_t), nHeight, nOrigHeight, nOvershoot, nUndershoot);
     }
 }
+
+template void clamp16_stacked_simd<CPU_SSE2>(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOrigHeight, int nOvershoot, int nUndershoot);
+template void clamp16_stacked_simd<CPU_SSE4_1>(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOrigHeight, int nOvershoot, int nUndershoot);
+
+template void clamp16_native_c<10>(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOrigHeight, int nOvershoot, int nUndershoot);
+template void clamp16_native_c<12>(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOrigHeight, int nOvershoot, int nUndershoot);
+template void clamp16_native_c<14>(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOrigHeight, int nOvershoot, int nUndershoot);
+template void clamp16_native_c<16>(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOrigHeight, int nOvershoot, int nUndershoot);
+
+template void clamp16_native_simd<CPU_SSE2,10>(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOrigHeight, int nOvershoot, int nUndershoot);
+template void clamp16_native_simd<CPU_SSE2,12>(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOrigHeight, int nOvershoot, int nUndershoot);
+template void clamp16_native_simd<CPU_SSE2,14>(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOrigHeight, int nOvershoot, int nUndershoot);
+template void clamp16_native_simd<CPU_SSE2,16>(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOrigHeight, int nOvershoot, int nUndershoot);
+
+template void clamp16_native_simd<CPU_SSE4_1,10>(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOrigHeight, int nOvershoot, int nUndershoot);
+template void clamp16_native_simd<CPU_SSE4_1,12>(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOrigHeight, int nOvershoot, int nUndershoot);
+template void clamp16_native_simd<CPU_SSE4_1,14>(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOrigHeight, int nOvershoot, int nUndershoot);
+template void clamp16_native_simd<CPU_SSE4_1,16>(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOrigHeight, int nOvershoot, int nUndershoot);
+
+Processor *clamp16_stacked_sse2 = &clamp16_stacked_simd<CPU_SSE2>;
+Processor *clamp16_stacked_sse4_1 = &clamp16_stacked_simd<CPU_SSE4_1>;
+
+Processor *clamp16_native_10_c = &clamp16_native_c<10>;
+Processor *clamp16_native_12_c = &clamp16_native_c<12>;
+Processor *clamp16_native_14_c = &clamp16_native_c<14>;
+Processor *clamp16_native_16_c = &clamp16_native_c<16>;
+
+Processor *clamp16_native_10_sse2 = &clamp16_native_simd<CPU_SSE2,10>;
+Processor *clamp16_native_10_sse4_1 = &clamp16_native_simd<CPU_SSE4_1,10>;
+Processor *clamp16_native_12_sse2 = &clamp16_native_simd<CPU_SSE2, 12>;
+Processor *clamp16_native_12_sse4_1 = &clamp16_native_simd<CPU_SSE4_1, 12>;
+Processor *clamp16_native_14_sse2 = &clamp16_native_simd<CPU_SSE2, 14>;
+Processor *clamp16_native_14_sse4_1 = &clamp16_native_simd<CPU_SSE4_1, 14>;
+Processor *clamp16_native_16_sse2 = &clamp16_native_simd<CPU_SSE2, 16>;
+Processor *clamp16_native_16_sse4_1 = &clamp16_native_simd<CPU_SSE4_1, 16>;
 
 
 
--- masktools/filters/support/clamp16/clamp16.h
+++ masktools/filters/support/clamp16/clamp16.h
@@ -5,13 +5,26 @@
 
 namespace Filtering { namespace MaskTools { namespace Filters { namespace Support { namespace Clamp16 {
 
-typedef void(Processor)(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOvershoot, int nUndershoot);
+typedef void(Processor)(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pUpLimit, ptrdiff_t nUpLimitPitch, const Byte *pLowLimit, ptrdiff_t nLowLimitPitch, int nWidth, int nHeight, int nOrigHeight, int nOvershoot, int nUndershoot);
 
 Processor clamp16_stacked_c;
-Processor clamp16_stacked_sse2;
+extern Processor *clamp16_stacked_sse2;
+extern Processor *clamp16_stacked_sse4_1;
 
-Processor clamp16_interleaved_c;
-Processor clamp16_interleaved_sse2;
+extern Processor *clamp16_native_10_c;
+extern Processor *clamp16_native_12_c;
+extern Processor *clamp16_native_14_c;
+extern Processor *clamp16_native_16_c;
+
+extern Processor *clamp16_native_10_sse2;
+extern Processor *clamp16_native_12_sse2;
+extern Processor *clamp16_native_14_sse2;
+extern Processor *clamp16_native_16_sse2;
+
+extern Processor *clamp16_native_10_sse4_1;
+extern Processor *clamp16_native_12_sse4_1;
+extern Processor *clamp16_native_14_sse4_1;
+extern Processor *clamp16_native_16_sse4_1;
 
 class Clamp16 : public MaskTools::Filter
 {
@@ -27,22 +40,59 @@ protected:
       processors.best_processor( constraints[nPlane] )( dst.data(), dst.pitch(), 
           frames[0].plane(nPlane).data(), frames[0].plane(nPlane).pitch(), 
           frames[1].plane(nPlane).data(), frames[1].plane(nPlane).pitch(), 
-          dst.width(), dst.height(), nOvershoot, nUndershoot );
+          dst.width(), dst.height(), dst.origheight(), nOvershoot, nUndershoot );
    }
 
 public:
     Clamp16(const Parameters &parameters) : MaskTools::Filter( parameters, FilterProcessingType::INPLACE )
     {
+      bool isStacked = parameters["stacked"].toBool();
+      int bits_per_pixel = bit_depths[C];
+
+      if (isStacked && bits_per_pixel != 8) {
+        error = "Stacked specified for a non-8 bit clip";
+        return;
+      }
+      if (!isStacked && bits_per_pixel == 8) {
+        error = "8 bit clip needs stacked=true";
+        return;
+      }
+      if (bits_per_pixel == 32) {
+        error = "32 bit float clip is not supported yet";
+        return;
+      }
+      
         nUndershoot = parameters["undershoot"].toInt();
         nOvershoot = parameters["overshoot"].toInt();
 
         /* add the processors */
-        if (parameters["stacked"].toBool() == true) {
+        if (isStacked) {
             processors.push_back(Filtering::Processor<Processor>(clamp16_stacked_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0));
             processors.push_back(Filtering::Processor<Processor>(clamp16_stacked_sse2, Constraint(CPU_SSE2, 1, 1, 1, 1), 1));
+            processors.push_back(Filtering::Processor<Processor>(clamp16_stacked_sse4_1, Constraint(CPU_SSE4_1, 1, 1, 1, 1), 2));
         } else {
-            processors.push_back(Filtering::Processor<Processor>(clamp16_interleaved_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0));
-            processors.push_back(Filtering::Processor<Processor>(clamp16_interleaved_sse2, Constraint(CPU_SSE2, 1, 1, 1, 1), 1));
+          switch (bit_depths[C]) {
+          case 10:
+            processors.push_back(Filtering::Processor<Processor>(clamp16_native_10_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0));
+            processors.push_back(Filtering::Processor<Processor>(clamp16_native_10_sse2, Constraint(CPU_SSE2, 1, 1, 1, 1), 1));
+            processors.push_back(Filtering::Processor<Processor>(clamp16_native_10_sse4_1, Constraint(CPU_SSE4_1, 1, 1, 1, 1), 2));
+            break;
+          case 12:
+            processors.push_back(Filtering::Processor<Processor>(clamp16_native_12_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0));
+            processors.push_back(Filtering::Processor<Processor>(clamp16_native_12_sse2, Constraint(CPU_SSE2, 1, 1, 1, 1), 1));
+            processors.push_back(Filtering::Processor<Processor>(clamp16_native_12_sse4_1, Constraint(CPU_SSE4_1, 1, 1, 1, 1), 2));
+            break;
+          case 14:
+            processors.push_back(Filtering::Processor<Processor>(clamp16_native_14_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0));
+            processors.push_back(Filtering::Processor<Processor>(clamp16_native_14_sse2, Constraint(CPU_SSE2, 1, 1, 1, 1), 1));
+            processors.push_back(Filtering::Processor<Processor>(clamp16_native_14_sse4_1, Constraint(CPU_SSE4_1, 1, 1, 1, 1), 2));
+            break;
+          case 16:
+            processors.push_back(Filtering::Processor<Processor>(clamp16_native_16_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0));
+            processors.push_back(Filtering::Processor<Processor>(clamp16_native_16_sse2, Constraint(CPU_SSE2, 1, 1, 1, 1), 1));
+            processors.push_back(Filtering::Processor<Processor>(clamp16_native_16_sse4_1, Constraint(CPU_SSE4_1, 1, 1, 1, 1), 2));
+            break;
+          }
         }
     }
 
