ae62acccf2e15940eeeb3f6a58bccc259f27814f
Pinterf <Pinterf@gmail.com>
2017-02-15 13:47:02 +0100

    mt_lutspa: 10-16 bit and 32 bit float

--- masktools/filters/lut/lutspa/lutspa.h
+++ masktools/filters/lut/lutspa/lutspa.h
@@ -13,17 +13,23 @@ Processor lut_c;
 class Lutspa : public MaskTools::Filter
 {
    Byte *luts[3];
+   int bits_per_pixel;
+   int pixelsize;
 
 protected:
     virtual void process(int n, const Plane<Byte> &dst, int nPlane, const Filtering::Frame<const Byte> frames[3], const Constraint constraints[3]) override
     {
         UNUSED(n); UNUSED(frames); UNUSED(constraints);
-        Functions::copy_plane(dst.data(), dst.pitch(), luts[nPlane], dst.width(), dst.width(), dst.height());
+        Functions::copy_plane(dst.data(), dst.pitch(), luts[nPlane], dst.width() * pixelsize, dst.width() * pixelsize, dst.height());
+        // copy_plane needs row_size in bytes
     }
 
 public:
    Lutspa(const Parameters &parameters) : MaskTools::Filter(parameters, FilterProcessingType::CHILD)
    {
+      bits_per_pixel = bit_depths[C];
+      pixelsize = bits_per_pixel == 8 ? 1 : (bits_per_pixel <= 16) ? 2 : 4;
+
       static const char *expr_strs[] = { "yExpr", "uExpr", "vExpr" };
       bool is_relative = parameters["relative"].toBool();
       bool is_biased = parameters["biased"].toBool();
@@ -69,11 +75,55 @@ public:
             return;
          }
 
-         luts[i] = reinterpret_cast<Byte*>(_aligned_malloc(w*h, 16));
+         luts[i] = reinterpret_cast<Byte*>(_aligned_malloc(w*h*pixelsize, 16));
 
-         for ( int x = 0; x < w; x++ )
-            for ( int y = 0; y < h; y++ )
-               luts[i][x+y*w] = is_relative ? ctx.compute_byte(x * 1.0 / ((is_biased || w < 2) ? w : w - 1), y * 1.0 / ((is_biased || h < 2) ? h : h - 1)) : ctx.compute_byte(x, y);
+         if (bits_per_pixel == 8) {
+           for (int x = 0; x < w; x++)
+             for (int y = 0; y < h; y++)
+               luts[i][x + y*w] = is_relative ?
+               ctx.compute_byte(x * 1.0 / ((is_biased || w < 2) ? w : w - 1),
+                 y * 1.0 / ((is_biased || h < 2) ? h : h - 1),
+                 -1, /* n/a */
+                 bits_per_pixel // new: bit-depth goes to param B
+               ) : ctx.compute_byte(x, y, -1 /*n/a*/, bits_per_pixel);
+         }
+         else if (bits_per_pixel < 16) { // clamp to max_pixel_value
+           uint16_t max_pixel_value = (1 << bits_per_pixel) - 1;
+           for (int x = 0; x < w; x++)
+             for (int y = 0; y < h; y++) {
+               uint16_t val = is_relative ?
+                 ctx.compute_word(x * 1.0 / ((is_biased || w < 2) ? w : w - 1),
+                   y * 1.0 / ((is_biased || h < 2) ? h : h - 1),
+                   -1, /* n/a */
+                   bits_per_pixel // new: bit-depth goes to param B
+                 ) : ctx.compute_word(x, y, -1 /*n/a*/, bits_per_pixel);
+               reinterpret_cast<uint16_t *>(luts[i])[x + y*w] = min(val, max_pixel_value);
+             }
+         }
+         else if (bits_per_pixel == 16) {
+           for (int x = 0; x < w; x++)
+             for (int y = 0; y < h; y++) {
+               uint16_t val = is_relative ?
+                 ctx.compute_word(x * 1.0 / ((is_biased || w < 2) ? w : w - 1),
+                   y * 1.0 / ((is_biased || h < 2) ? h : h - 1),
+                   -1, /* n/a */
+                   bits_per_pixel // new: bit-depth goes to param B
+                 ) : ctx.compute_word(x, y, -1 /*n/a*/, bits_per_pixel);
+               reinterpret_cast<uint16_t *>(luts[i])[x + y*w] = val;
+             }
+         }
+         else { // float
+           for (int x = 0; x < w; x++)
+             for (int y = 0; y < h; y++) {
+               float val = is_relative ?
+                 ctx.compute_float(x * 1.0 / ((is_biased || w < 2) ? w : w - 1),
+                   y * 1.0 / ((is_biased || h < 2) ? h : h - 1),
+                   -1, /* n/a */
+                   bits_per_pixel // new: bit-depth goes to param B
+                 ) : ctx.compute_float(x, y, -1 /*n/a*/, bits_per_pixel);
+               reinterpret_cast<float *>(luts[i])[x + y*w] = val;
+             }
+         }
       }
    }
 
