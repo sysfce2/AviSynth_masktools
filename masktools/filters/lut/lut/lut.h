b580634265c7c48ea437efa503d79704a61db6b9
Pinterf <Pinterf@gmail.com>
2017-02-16 21:01:01 +0100

    mt_lut: realtime option for 8-16 bits, 32 bit is realtime only

--- masktools/filters/lut/lut/lut.h
+++ masktools/filters/lut/lut/lut.h
@@ -8,18 +8,36 @@ namespace Filtering { namespace MaskTools { namespace Filters { namespace Lut { 
 
 typedef void(Processor)(Byte *pDst, ptrdiff_t nDstPitch, int nWidth, int nHeight, const Byte lut[256]);
 typedef void(Processor16)(Byte *pDst, ptrdiff_t nDstPitch, int nWidth, int nHeight, const Word lut[65536], int nOrigHeightForStacked);
+typedef void(ProcessorCtx)(Byte *pDst, ptrdiff_t nDstPitch, int nWidth, int nHeight, Parser::Context *ctx);
 
 Processor lut_c;
 
 Processor16 lut16_c_native;
 Processor16 lut16_c_stacked;
 
+ProcessorCtx realtime8_c;
+extern ProcessorCtx *realtime10_c;
+extern ProcessorCtx *realtime12_c;
+extern ProcessorCtx *realtime14_c;
+extern ProcessorCtx *realtime16_c;
+ProcessorCtx realtime32_c;
+
 class Lut : public MaskTools::Filter
 {
    Byte luts[3][256];
    Word *luts16[3]; // full size, even for 10 bits (avoid over addressing by invalid pixel values)
+
+   // for realtime
+   std::deque<Filtering::Parser::Symbol> *parsed_expressions[3];
+
    Processor *processor;
    Processor16 *processor16;
+   ProcessorCtx *processorCtx;
+   ProcessorCtx *processorCtx16;
+   ProcessorCtx *processorCtx32;
+   int bits_per_pixel;
+   bool isStacked;
+   bool realtime;
 
 protected:
     virtual void process(int n, const Plane<Byte> &dst, int nPlane, const ::Filtering::Frame<const Byte> frames[3], const Constraint constraints[3]) override
@@ -27,31 +45,31 @@ protected:
         UNUSED(n);
         UNUSED(constraints);
         UNUSED(frames);
-        int bits_per_pixel = bit_depths[C];
-        if (parameters["stacked"].toBool())
-          bits_per_pixel = 16;
-        if(bits_per_pixel==8)
+        if (realtime) {
+          // thread safety
+          Parser::Context ctx(*parsed_expressions[nPlane]);
+          processorCtx(dst.data(), dst.pitch(), dst.width(), dst.height(), &ctx);
+        }
+        else if (bits_per_pixel == 8)
           processor(dst.data(), dst.pitch(), dst.width(), dst.height(), luts[nPlane]);
         else if (bits_per_pixel <= 16)
           processor16(dst.data(), dst.pitch(), dst.width(), dst.height(), luts16[nPlane], dst.origheight());
-        else {
-          // no float lut here
-          // todo: expr pixel by pixel
-        }
     }
 
 public:
    Lut(const Parameters &parameters) : MaskTools::Filter( parameters, FilterProcessingType::INPLACE )
    {
-      for (int i = 0; i < 3; i++)
+     for (int i = 0; i < 3; i++) {
         luts16[i] = nullptr;
+        parsed_expressions[i] = nullptr;
+      }
 
       static const char *expr_strs[] = { "yExpr", "uExpr", "vExpr" };
 
       Parser::Parser parser = Parser::getDefaultParser().addSymbol(Parser::Symbol::X);
 
-      bool isStacked = parameters["stacked"].toBool();
-      int bits_per_pixel = bit_depths[C];
+      isStacked = parameters["stacked"].toBool();
+      bits_per_pixel = bit_depths[C];
       int max_pixel_value = (1 << bits_per_pixel) - 1;
 
       if (isStacked && bits_per_pixel != 8) {
@@ -62,8 +80,14 @@ public:
       if (isStacked)
         bits_per_pixel = 16;
 
-      if (bits_per_pixel == 32) {
-        error = "32 bit float clip is not supported yet";
+      realtime = parameters["realtime"].toBool();
+
+      if (bits_per_pixel == 32) { // no lookup for float
+        realtime = true;
+      }
+
+      if (realtime && isStacked) {
+        error = "realtime calculation not supported for stacked clip";
         return;
       }
 
@@ -92,6 +116,21 @@ public:
           return;
         }
 
+        if (realtime) {
+          parsed_expressions[i] = new std::deque<Parser::Symbol>(parser.getExpression());
+
+          switch (bits_per_pixel) {
+          case 8: processorCtx = realtime8_c; break;
+          case 10: processorCtx = realtime10_c; break;
+          case 12: processorCtx = realtime12_c; break;
+          case 14: processorCtx = realtime14_c; break;
+          case 16: processorCtx = realtime16_c; break;
+          case 32: processorCtx = realtime32_c; break;
+          }
+          continue;
+        }
+
+        // real lut
         if(bits_per_pixel >=8 && bits_per_pixel <= 16) 
           luts16[i] = reinterpret_cast<Word*>(_aligned_malloc(65536*sizeof(uint16_t), 16));
 
@@ -126,6 +165,7 @@ public:
    {
      for (int i = 0; i < 3; i++) {
        _aligned_free(luts16[i]);
+       delete parsed_expressions[i];
      }
    }
 
@@ -141,6 +181,7 @@ public:
       signature.add(Parameter(String("x"), "uExpr"));
       signature.add(Parameter(String("x"), "vExpr"));
       signature.add(Parameter(false, "stacked"));
+      signature.add(Parameter(false, "realtime"));
 
       return add_defaults( signature );
    }
