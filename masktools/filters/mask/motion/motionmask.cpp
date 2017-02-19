9b15a0bdb7c6d64494039ceaef025e42c27dca4b
Pinterf <Pinterf@gmail.com>
2017-02-19 23:38:34 +0100

    mt_motion 10-16 bit/float

--- masktools/filters/mask/motion/motionmask.cpp
+++ masktools/filters/mask/motion/motionmask.cpp
@@ -4,63 +4,67 @@
 
 namespace Filtering { namespace MaskTools { namespace Filters { namespace Mask { namespace Motion {
 
-static unsigned int sad_c_op(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nWidth, int nHeight)
-{
-   unsigned int nSad = 0;
-	for ( int y = 0; y < nHeight; y++ ) 
-	{
-		for (int x = 0; x < nWidth; x++ )
-         nSad += abs(pDst[x] - pSrc[x]);
+  static uint64_t sad_c_op(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nWidth, int nHeight)
+  {
+    uint64_t nSad = 0;
+    for (int y = 0; y < nHeight; y++)
+    {
+      unsigned int nSad32 = 0;
+      for (int x = 0; x < nWidth; x++)
+        nSad32 += abs(pDst[x] - pSrc[x]);
+      nSad += nSad32; // avoid overflow for big frames
       pDst += nDstPitch;
       pSrc += nSrcPitch;
-	}
-   return nSad;
-}
-
-static void mask_c_op(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nLowThreshold, int nHighThreshold, int nWidth, int nHeight)
-{
-	for ( int y = 0; y < nHeight; y++ ) 
-	{
-		for (int x = 0; x < nWidth; x++ )
-         pDst[x] = threshold<Byte, int>( abs<int>( pDst[x] - pSrc[x] ), nLowThreshold, nHighThreshold );
-      pDst += nDstPitch;
-      pSrc += nSrcPitch;
-   }
-}
-
-
-template <MemoryMode mem_mode>
-static unsigned int sad_sse2_op(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nWidth, int nHeight)
-{
-    int wMod16 = (nWidth / 16) * 16;
-    auto pDst2 = pDst;
-    auto pSrc2 = pSrc;
-    __m128i acc = _mm_setzero_si128();
-    for ( int j = 0; j < nHeight; ++j ) {
-        for ( int i = 0; i < wMod16; i+=16 ) {
-            auto dst1 = simd_load_si128<mem_mode>(pDst+i);
-            auto src1 = simd_load_si128<mem_mode>(pSrc+i);
-
-            auto sad1 = _mm_sad_epu8(dst1, src1);
-
-            acc = _mm_add_epi32(acc, sad1);
-        }
-        pDst += nDstPitch;
-        pSrc += nSrcPitch;
     }
-    
-    auto idk = _mm_castps_si128(_mm_movehl_ps(_mm_setzero_ps(), _mm_castsi128_ps(acc)));
-    auto sum = _mm_add_epi32(acc, idk);
-    auto sad = _mm_cvtsi128_si32(sum);
+    return nSad;
+  }
+
+  static void mask_c_op(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nLowThreshold, int nHighThreshold, int nWidth, int nHeight)
+  {
+    for (int y = 0; y < nHeight; y++)
+    {
+      for (int x = 0; x < nWidth; x++)
+        pDst[x] = threshold<Byte, int>(abs<int>(pDst[x] - pSrc[x]), nLowThreshold, nHighThreshold);
+      pDst += nDstPitch;
+      pSrc += nSrcPitch;
+    }
+  }
+
+
+  template <MemoryMode mem_mode>
+  static uint64_t sad_sse2_op(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nWidth, int nHeight)
+  {
+    uint64_t sad = 0; // avoid overflow
+    int wMod16 = (nWidth / 16) * 16;
+    auto pDstSave = pDst;
+    auto pSrcSave = pSrc;
+    for (int j = 0; j < nHeight; ++j) {
+      __m128i acc = _mm_setzero_si128();
+      for (int i = 0; i < wMod16; i += 16) {
+        auto dst1 = simd_load_si128<mem_mode>(pDst + i);
+        auto src1 = simd_load_si128<mem_mode>(pSrc + i);
+
+        auto sad1 = _mm_sad_epu8(dst1, src1);
+
+        acc = _mm_add_epi32(acc, sad1);
+      }
+      auto idk = _mm_castps_si128(_mm_movehl_ps(_mm_setzero_ps(), _mm_castsi128_ps(acc)));
+      auto sum = _mm_add_epi32(acc, idk);
+      auto sad32 = _mm_cvtsi128_si32(sum);
+      sad += sad32;
+      pDst += nDstPitch;
+      pSrc += nSrcPitch;
+    }
+
     if (nWidth > wMod16) {
-        sad += sad_c_op(pDst2 + wMod16, nDstPitch, pSrc2 + wMod16, nSrcPitch, nWidth - wMod16, nHeight);
+      sad += sad_c_op(pDstSave + wMod16, nDstPitch, pSrcSave + wMod16, nSrcPitch, nWidth - wMod16, nHeight);
     }
     return sad;
-}
+  }
 
-template <MemoryMode mem_mode>
-static void mask_sse2_op(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nLowThreshold, int nHighThreshold, int nWidth, int nHeight)
-{
+  template <MemoryMode mem_mode>
+  static void mask_sse2_op(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nLowThreshold, int nHighThreshold, int nWidth, int nHeight)
+  {
     int wMod16 = (nWidth / 16) * 16;
     auto pDst2 = pDst;
     auto pSrc2 = pSrc;
@@ -71,32 +75,33 @@ static void mask_sse2_op(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrd
     lowThresh = _mm_sub_epi8(lowThresh, v128);
     highThresh = _mm_sub_epi8(highThresh, v128);
 
-    for ( int j = 0; j < nHeight; ++j ) {
-        for ( int i = 0; i < wMod16; i+=16 ) {
-            auto dst1 = simd_load_si128<mem_mode>(pDst+i);
-            auto src1 = simd_load_si128<mem_mode>(pSrc+i);
+    for (int j = 0; j < nHeight; ++j) {
+      for (int i = 0; i < wMod16; i += 16) {
+        auto dst1 = simd_load_si128<mem_mode>(pDst + i);
+        auto src1 = simd_load_si128<mem_mode>(pSrc + i);
 
-            auto greater = _mm_subs_epu8(dst1, src1);
-            auto smaller = _mm_subs_epu8(src1, dst1);
-            auto diff = _mm_add_epi8(greater, smaller);
+        auto greater = _mm_subs_epu8(dst1, src1);
+        auto smaller = _mm_subs_epu8(src1, dst1);
+        auto diff = _mm_add_epi8(greater, smaller);
 
-            auto mask = threshold_sse2(diff, lowThresh, highThresh, v128);
+        auto mask = threshold_sse2(diff, lowThresh, highThresh, v128);
 
-            simd_store_si128<mem_mode>(pDst + i, mask);
-        }
-        pDst += nDstPitch;
-        pSrc += nSrcPitch;
+        simd_store_si128<mem_mode>(pDst + i, mask);
+      }
+      pDst += nDstPitch;
+      pSrc += nSrcPitch;
     }
 
     if (nWidth > wMod16) {
-        mask_c_op(pDst2 + wMod16, nDstPitch, pSrc2 + wMod16, nSrcPitch, nLowThreshold, nHighThreshold, nWidth - wMod16, nHeight);
+      mask_c_op(pDst2 + wMod16, nDstPitch, pSrc2 + wMod16, nSrcPitch, nLowThreshold, nHighThreshold, nWidth - wMod16, nHeight);
     }
-}
+  }
+
 
 template <decltype(sad_c_op) sad, decltype(mask_c_op) mask>
 bool mask_t(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nLowThreshold, int nHighThreshold, int nMotionThreshold, int nSceneChange, int nSceneChangeValue, int nWidth, int nHeight)
 {
-    bool scenechange = nSceneChange >= 2 ? nSceneChange == 3 : sad(pDst, nDstPitch, pSrc, nSrcPitch, nWidth, nHeight) > (unsigned int)(nMotionThreshold * nWidth * nHeight);
+    bool scenechange = nSceneChange >= 2 ? nSceneChange == 3 : sad(pDst, nDstPitch, pSrc, nSrcPitch, nWidth, nHeight) > (uint64_t)(nMotionThreshold * nWidth * nHeight);
 
     if (scenechange) {
         Functions::memset_plane(pDst, nDstPitch, nWidth, nHeight, static_cast<Byte>(nSceneChangeValue));
@@ -107,7 +112,7 @@ bool mask_t(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPit
 }
 
 
-Processor *mask_c       = &mask_t<sad_c_op, mask_c_op>;
+Processor *mask_c    = &mask_t<sad_c_op, mask_c_op>;
 Processor *mask_sse2 = &mask_t<sad_sse2_op<MemoryMode::SSE2_UNALIGNED>, mask_sse2_op<MemoryMode::SSE2_UNALIGNED>>;
 Processor *mask_asse2 = &mask_t<sad_sse2_op<MemoryMode::SSE2_ALIGNED>, mask_sse2_op<MemoryMode::SSE2_ALIGNED>>;
 
--- masktools/filters/mask/motion/motionmask.h
+++ masktools/filters/mask/motion/motionmask.h
@@ -6,10 +6,20 @@
 namespace Filtering { namespace MaskTools { namespace Filters { namespace Mask { namespace Motion {
 
 typedef bool (Processor)(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nLowThreshold, int nHighThreshold, int nMotionThreshold, int nSceneChange, int nSceneChangeValue, int nWidth, int nHeight);
+typedef bool (Processor16)(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nLowThreshold, int nHighThreshold, int nMotionThreshold, int nSceneChange, int nSceneChangeValue, int nWidth, int nHeight);
+typedef bool (Processor32)(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, Float nLowThreshold, Float nHighThreshold, Float nMotionThreshold, int nSceneChange, Float nSceneChangeValue_f, int nWidth, int nHeight);
 
 extern Processor *mask_c;
 extern Processor *mask_sse2;
 extern Processor *mask_asse2;
+
+extern Processor16 *mask10_16_c;
+extern Processor16 *mask10_16_sse2;
+extern Processor16 *mask10_16_asse2;
+
+extern Processor32 *mask32_c;
+extern Processor32 *mask32_sse2;
+extern Processor32 *mask32_asse2;
 
 class MotionMask : public MaskTools::Filter
 {
@@ -17,46 +27,128 @@ class MotionMask : public MaskTools::Filter
    int nHighThresholds[3];
    int nMotionThreshold;
 
-   int nSceneChange;
-   int nSceneChangeValue;
+   float nLowThresholds_f[3];
+   float nHighThresholds_f[3];
+   float nMotionThreshold_f;
 
    ProcessorList<Processor> processors;
+   ProcessorList<Processor16> processors16;
+   ProcessorList<Processor32> processors32;
+
+   int nSceneChange;
+   
+   int nSceneChangeValue;
+   Float nSceneChangeValue_f;
+
+   int bits_per_pixel;
 
 protected:
 
     virtual void process(int n, const Plane<Byte> &dst, int nPlane, const Filtering::Frame<const Byte> frames[3], const Constraint constraints[3]) override
     {
         UNUSED(n);
-        if (nPlane == 0)
+        if (bits_per_pixel == 8) {
+          if (nPlane == 0)
             nSceneChange = processors.best_processor(constraints[nPlane])(dst.data(), dst.pitch(),
-            frames[0].plane(nPlane).data(), frames[0].plane(nPlane).pitch(),
-            nLowThresholds[nPlane], nHighThresholds[nPlane], nMotionThreshold, 0, nSceneChangeValue, dst.width(), dst.height()) ? 3 : 2;
-        else
+              frames[0].plane(nPlane).data(), frames[0].plane(nPlane).pitch(),
+              nLowThresholds[nPlane], nHighThresholds[nPlane], nMotionThreshold, 0, nSceneChangeValue, dst.width(), dst.height()) ? 3 : 2;
+          else
             processors.best_processor(constraints[nPlane])(dst.data(), dst.pitch(),
-            frames[0].plane(nPlane).data(), frames[0].plane(nPlane).pitch(),
-            nLowThresholds[nPlane], nHighThresholds[nPlane], nMotionThreshold, nSceneChange, nSceneChangeValue, dst.width(), dst.height());
+              frames[0].plane(nPlane).data(), frames[0].plane(nPlane).pitch(),
+              nLowThresholds[nPlane], nHighThresholds[nPlane], nMotionThreshold, nSceneChange, nSceneChangeValue, dst.width(), dst.height());
+        }
+        else if (bits_per_pixel <= 16) {
+          if (nPlane == 0)
+            nSceneChange = processors16.best_processor(constraints[nPlane])(dst.data(), dst.pitch(),
+              frames[0].plane(nPlane).data(), frames[0].plane(nPlane).pitch(),
+              nLowThresholds[nPlane], nHighThresholds[nPlane], nMotionThreshold, 0, nSceneChangeValue, dst.width(), dst.height()) ? 3 : 2;
+          else
+            processors16.best_processor(constraints[nPlane])(dst.data(), dst.pitch(),
+              frames[0].plane(nPlane).data(), frames[0].plane(nPlane).pitch(),
+              nLowThresholds[nPlane], nHighThresholds[nPlane], nMotionThreshold, nSceneChange, nSceneChangeValue, dst.width(), dst.height());
+        }
+        else {
+          if (nPlane == 0)
+            nSceneChange = processors32.best_processor(constraints[nPlane])(dst.data(), dst.pitch(),
+              frames[0].plane(nPlane).data(), frames[0].plane(nPlane).pitch(),
+              nLowThresholds_f[nPlane], nHighThresholds_f[nPlane], nMotionThreshold_f, 0, nSceneChangeValue_f, dst.width(), dst.height()) ? 3 : 2;
+          else
+            processors32.best_processor(constraints[nPlane])(dst.data(), dst.pitch(),
+              frames[0].plane(nPlane).data(), frames[0].plane(nPlane).pitch(),
+              nLowThresholds_f[nPlane], nHighThresholds_f[nPlane], nMotionThreshold_f, nSceneChange, nSceneChangeValue_f, dst.width(), dst.height());
+        }
     }
 
 public:
    MotionMask(const Parameters &parameters) : MaskTools::Filter( parameters, FilterProcessingType::INPLACE )
    {
-      if (bit_depths[C] != 8) {
-        error = "only 8 bit clip accepted"; // todo: 10-16bit, float
-        return;
-      }
+     bits_per_pixel = bit_depths[C];
+     bool isFloat = bits_per_pixel == 32;
 
-      nLowThresholds[0] = clip<int, int>( parameters["thY1"].toInt(), 0, 255 );
-      nLowThresholds[1] = nLowThresholds[2] = clip<int, int>( parameters["thC1"].toInt(), 0, 255 );
-      nHighThresholds[0] = clip<int, int>( parameters["thY2"].toInt(), 0, 255 );
-      nHighThresholds[1] = nHighThresholds[2] = clip<int, int>( parameters["thC2"].toInt(), 0, 255 );
-      nMotionThreshold = clip<int, int>( parameters["thT"].toInt(), 0, 255 );
-      nSceneChangeValue = clip<int, int>( parameters["scvalue"].toInt(), 0, 255 );
+
+     if (!isFloat) {
+       // default value of 10 scaled by bit depth
+       int nLow0, nLow1;
+       int nHigh0, nHigh1;
+       int max_pixel_value = (1 << bits_per_pixel) - 1;
+       nLow0 = parameters["thY1"].is_defined() ? parameters["thY1"].toInt() : (10 << (bits_per_pixel - 8));
+       nLow1 = parameters["thC1"].is_defined() ? parameters["thC1"].toInt() : (10 << (bits_per_pixel - 8));
+       nHigh0 = parameters["thY2"].is_defined() ? parameters["thY2"].toInt() : (10 << (bits_per_pixel - 8));
+       nHigh1 = parameters["thC2"].is_defined() ? parameters["thC2"].toInt() : (10 << (bits_per_pixel - 8));
+
+       nMotionThreshold = parameters["thT"].is_defined() ? parameters["thT"].toInt() : (10 << (bits_per_pixel - 8));
+       nMotionThreshold = min(max(nMotionThreshold, 0), max_pixel_value);
+
+       nLowThresholds[0] = min(max(nLow0, 0), max_pixel_value);
+       nLowThresholds[1] = nLowThresholds[2] = min(max(nLow1, 0), max_pixel_value);
+       nHighThresholds[0] = min(max(nHigh0, 0), max_pixel_value);
+       nHighThresholds[1] = nHighThresholds[2] = min(max(nHigh1, 0), max_pixel_value);
+
+       // value copied into the whole frame when scenechange is detected
+       nSceneChangeValue = parameters["scvalue"].is_defined() ? parameters["scvalue"].toInt() : 0;
+       nSceneChangeValue = min(max(nSceneChangeValue, 0), max_pixel_value);
+     }
+     else {
+       float nLow0, nLow1;
+       float nHigh0, nHigh1;
+       nLow0 = parameters["thY1"].is_defined() ? (float)parameters["thY1"].toFloat() : (10.0f / 255);
+       nLow1 = parameters["thC1"].is_defined() ? (float)parameters["thC1"].toFloat() : (10.0f / 255);
+       nHigh0 = parameters["thY2"].is_defined() ? (float)parameters["thY2"].toFloat() : (10.0f / 255);
+       nHigh1 = parameters["thC2"].is_defined() ? (float)parameters["thC2"].toFloat() : (10.0f / 255);
+       
+       nMotionThreshold_f = parameters["thT"].is_defined() ? (float)parameters["thC2"].toFloat() : (10.0f / 255);
+
+       nLowThresholds_f[0] = nLow0;
+       nLowThresholds_f[1] = nLowThresholds_f[2] = nLow1;
+       nHighThresholds_f[0] = nHigh0;
+       nHighThresholds_f[1] = nHighThresholds_f[2] = nHigh1;
+
+       nSceneChangeValue_f = (Float)(parameters["scvalue"].toFloat());
+     }
+
 
       /* add the processors */
-      processors.push_back(Filtering::Processor<Processor>(mask_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0));
-      processors.push_back(Filtering::Processor<Processor>(mask_sse2, Constraint(CPU_SSE2, 1, 1, 1, 1), 1));
-      processors.push_back(Filtering::Processor<Processor>(mask_asse2, Constraint(CPU_SSE2, 1, 1, 16, 16), 2));
-   }
+      switch (bits_per_pixel) {
+      case 8:
+        processors.push_back(Filtering::Processor<Processor>(mask_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0));
+        processors.push_back(Filtering::Processor<Processor>(mask_sse2, Constraint(CPU_SSE2, 1, 1, 1, 1), 1));
+        processors.push_back(Filtering::Processor<Processor>(mask_asse2, Constraint(CPU_SSE2, 1, 1, 16, 16), 2));
+        break;
+      case 10:
+      case 12:
+      case 14:
+      case 16:
+        processors16.push_back(Filtering::Processor<Processor16>(mask10_16_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0));
+        processors16.push_back(Filtering::Processor<Processor16>(mask10_16_sse2, Constraint(CPU_SSE2, 1, 1, 1, 1), 1));
+        processors16.push_back(Filtering::Processor<Processor16>(mask10_16_asse2, Constraint(CPU_SSE2, 1, 1, 16, 16), 2));
+        break;
+      case 32:
+        processors32.push_back(Filtering::Processor<Processor32>(mask32_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0));
+        processors32.push_back(Filtering::Processor<Processor32>(mask32_sse2, Constraint(CPU_SSE2, 1, 1, 1, 1), 1)); // only sad is sse2
+        processors32.push_back(Filtering::Processor<Processor32>(mask32_asse2, Constraint(CPU_SSE2, 1, 1, 16, 16), 2));
+        break;
+      }
+      }
 
    InputConfiguration &input_configuration() const { return InPlaceTemporalOneFrame(); }
 
@@ -65,12 +157,12 @@ public:
       Signature signature = "mt_motion";
 
       signature.add(Parameter(TYPE_CLIP, ""));
-      signature.add(Parameter(10, "thY1"));
-      signature.add(Parameter(10, "thY2"));
-      signature.add(Parameter(10, "thC1"));
-      signature.add(Parameter(10, "thC2"));
-      signature.add(Parameter(10, "thT"));
-      signature.add(Parameter(0, "sc_value"));
+      signature.add(Parameter(10.0f, "thY1"));
+      signature.add(Parameter(10.0f, "thY2"));
+      signature.add(Parameter(10.0f, "thC1"));
+      signature.add(Parameter(10.0f, "thC2"));
+      signature.add(Parameter(10.0f, "thT"));
+      signature.add(Parameter(0.0f, "sc_value"));
 
       return add_defaults( signature );
    }
