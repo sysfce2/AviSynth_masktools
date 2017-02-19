c6af205ec7332d452295f6a488e929011ec79449
Pinterf <Pinterf@gmail.com>
2017-02-19 23:35:02 +0100

    mt_gradient: 10-16 bit / float

--- masktools/filters/gradient/gradient.cpp
+++ masktools/filters/gradient/gradient.cpp
@@ -1,11 +1,18 @@
 #include "gradient.h"
+#include <type_traits>
 
 using namespace Filtering;
 
+//template<typename pixel_t, typename sad_t>
+//using Distortion sad_t (*)(const pixel_t *pSrc, ptrdiff_t nSrcPitch, const pixel_t *pRef, ptrdiff_t nRefPitch, int nPrecision);
 typedef int (Distorsion)(const Byte *pSrc, ptrdiff_t nSrcPitch, const Byte *pRef, ptrdiff_t nRefPitch, int nPrecision);
+typedef int (Distorsion16)(const Word *pSrc, ptrdiff_t nSrcPitch, const Word *pRef, ptrdiff_t nRefPitch, int nPrecision);
+typedef float (Distorsion_f)(const Float *pSrc, ptrdiff_t nSrcPitch, const Float *pRef, ptrdiff_t nRefPitch, int nPrecision);
 
-template<int nBlockSizeX, int nBlockSizeY> int sad(const Byte *pSrc, ptrdiff_t nSrcPitch, const Byte *pRef, ptrdiff_t nRefPitch, int nPrecision)
+template<int nBlockSizeX, int nBlockSizeY> 
+int sad(const byte *pSrc, ptrdiff_t nSrcPitch, const byte *pRef, ptrdiff_t nRefPitch, int nPrecision)
 {
+  // pitches are already bit_depth corrected
    int nSad = 0;
 
    for ( int y = 0; y < nBlockSizeY; y++, pSrc += nSrcPitch * nPrecision, pRef += nRefPitch * nPrecision )
@@ -15,16 +22,46 @@ template<int nBlockSizeX, int nBlockSizeY> int sad(const Byte *pSrc, ptrdiff_t n
    return nSad;
 }
 
-template<Distorsion distorsion, int nBlockSizeX, int nBlockSizeY> void generic_c(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, const Byte *pRef, ptrdiff_t nRefPitch, int nWidth, int nHeight, int nX, int nY, int nMinimum, int nMaximum, int nPrecision)
+template<int nBlockSizeX, int nBlockSizeY>
+int sad16(const Word *pSrc, ptrdiff_t nSrcPitch, const Word *pRef, ptrdiff_t nRefPitch, int nPrecision)
 {
+  // pitches are already bit_depth corrected
+  int nSad = 0;
+
+  for (int y = 0; y < nBlockSizeY; y++, pSrc += nSrcPitch * nPrecision, pRef += nRefPitch * nPrecision)
+    for (int x = 0; x < nBlockSizeX; x++)
+      nSad += abs<int>(pSrc[x * nPrecision] - pRef[x * nPrecision]);
+
+  return nSad;
+}
+
+template<int nBlockSizeX, int nBlockSizeY>
+float sad32(const float *pSrc, ptrdiff_t nSrcPitch, const float *pRef, ptrdiff_t nRefPitch, int nPrecision)
+{
+  // pitches are already bit_depth corrected
+  float nSad = 0;
+
+  for (int y = 0; y < nBlockSizeY; y++, pSrc += nSrcPitch * nPrecision, pRef += nRefPitch * nPrecision)
+    for (int x = 0; x < nBlockSizeX; x++)
+      nSad += abs<float>(pSrc[x * nPrecision] - pRef[x * nPrecision]);
+
+  return nSad;
+}
+
+template<Distorsion distorsion, int nBlockSizeX, int nBlockSizeY> 
+void generic_c(byte *pDst, ptrdiff_t nDstPitch, const byte *pSrc, ptrdiff_t nSrcPitch, const byte *pRef, ptrdiff_t nRefPitch, int nWidth, int nHeight, int nX, int nY, int nMinimum, int nMaximum, int nPrecision)
+{
+ 
    int x, y;
 
    pSrc += nX + nY * nSrcPitch;
 
+   const int max_pixel_value = 255;
+
    for ( y = 0; y < nHeight - nBlockSizeY * nPrecision + nPrecision; y++, pRef += nRefPitch, pDst += nDstPitch )
    {
       for ( x = 0; x < nWidth - nBlockSizeX * nPrecision + nPrecision; x++ )
-         pDst[x] = clip<Byte, int>( (distorsion( pSrc, nSrcPitch, &pRef[x], nRefPitch, nPrecision ) - nMinimum) * 255 / (nMaximum - nMinimum) );
+         pDst[x] = clip<byte, int>( (distorsion( pSrc, nSrcPitch, &pRef[x], nRefPitch, nPrecision ) - nMinimum) * max_pixel_value / (nMaximum - nMinimum) );
 
       for ( ; x < nWidth; x++ )
          pDst[x] = pDst[nWidth - nBlockSizeX * nPrecision + nPrecision - 1];
@@ -33,8 +70,61 @@ template<Distorsion distorsion, int nBlockSizeX, int nBlockSizeY> void generic_c
       memcpy( &pDst[y * nDstPitch], pDst - nDstPitch, nWidth );
 }
 
+template<Distorsion16 distorsion, int nBlockSizeX, int nBlockSizeY, int bits_per_pixel>
+void generic16_c(Word *pDst, ptrdiff_t nDstPitch, const Word *pSrc, ptrdiff_t nSrcPitch, const Word *pRef, ptrdiff_t nRefPitch, int nWidth, int nHeight, int nX, int nY, int nMinimum, int nMaximum, int nPrecision)
+{
+  nSrcPitch /= sizeof(Word);
+  nDstPitch /= sizeof(Word);
+  nRefPitch /= sizeof(Word);
+
+  int x, y;
+
+  pSrc += nX + nY * nSrcPitch;
+
+  const int max_pixel_value = (1 << bits_per_pixel) - 1;
+
+  for (y = 0; y < nHeight - nBlockSizeY * nPrecision + nPrecision; y++, pRef += nRefPitch, pDst += nDstPitch)
+  {
+    for (x = 0; x < nWidth - nBlockSizeX * nPrecision + nPrecision; x++)
+      pDst[x] = clip<Word, int>((distorsion(pSrc, nSrcPitch, &pRef[x], nRefPitch, nPrecision) - nMinimum) * max_pixel_value / (nMaximum - nMinimum));
+
+    for (; x < nWidth; x++)
+      pDst[x] = pDst[nWidth - nBlockSizeX * nPrecision + nPrecision - 1];
+  }
+  for (y = 0; y < nBlockSizeY * nPrecision - nPrecision; y++)
+    memcpy(&pDst[y * nDstPitch], pDst - nDstPitch, nWidth * sizeof(Word));
+}
+
+template<Distorsion_f distorsion, int nBlockSizeX, int nBlockSizeY>
+void generic_f_c(Float *pDst, ptrdiff_t nDstPitch, const Float *pSrc, ptrdiff_t nSrcPitch, const Float *pRef, ptrdiff_t nRefPitch, int nWidth, int nHeight, int nX, int nY, Float nMinimum, Float nMaximum, int nPrecision)
+{
+  nSrcPitch /= sizeof(Float);
+  nDstPitch /= sizeof(Float);
+  nRefPitch /= sizeof(Float);
+
+  int x, y;
+
+  pSrc += nX + nY * nSrcPitch;
+
+  for (y = 0; y < nHeight - nBlockSizeY * nPrecision + nPrecision; y++, pRef += nRefPitch, pDst += nDstPitch)
+  {
+    for (x = 0; x < nWidth - nBlockSizeX * nPrecision + nPrecision; x++)
+      pDst[x] = (distorsion(pSrc, nSrcPitch, &pRef[x], nRefPitch, nPrecision) - nMinimum) / (nMaximum - nMinimum);
+
+    for (; x < nWidth; x++)
+      pDst[x] = pDst[nWidth - nBlockSizeX * nPrecision + nPrecision - 1];
+  }
+  for (y = 0; y < nBlockSizeY * nPrecision - nPrecision; y++)
+    memcpy(&pDst[y * nDstPitch], pDst - nDstPitch, nWidth * sizeof(Float));
+}
+
 namespace Filtering { namespace MaskTools { namespace Filters { namespace Gradient {
 
 Processor *sad_c  = &generic_c<sad<16, 16>, 16, 16>;
+Processor16 *sad_10_c = &generic16_c<sad16<16, 16>, 16, 16, 10>;
+Processor16 *sad_12_c = &generic16_c<sad16<16, 16>, 16, 16, 12>;
+Processor16 *sad_14_c = &generic16_c<sad16<16, 16>, 16, 16, 14>;
+Processor16 *sad_16_c = &generic16_c<sad16<16, 16>, 16, 16, 16>;
+Processor32 *sad_32_c = &generic_f_c<sad32<16, 16>, 16, 16>;
 
 } } } }
--- masktools/filters/gradient/gradient.h
+++ masktools/filters/gradient/gradient.h
@@ -6,44 +6,78 @@
 namespace Filtering { namespace MaskTools { namespace Filters { namespace Gradient {
 
 typedef void(Processor)(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, const Byte *pRef, ptrdiff_t nRefPitch, int nWidth, int nHeight, int nX, int nY, int nMinimum, int nMaximum, int nPrecision);
+typedef void(Processor16)(Word *pDst, ptrdiff_t nDstPitch, const Word *pSrc, ptrdiff_t nSrcPitch, const Word *pRef, ptrdiff_t nRefPitch, int nWidth, int nHeight, int nX, int nY, int nMinimum, int nMaximum, int nPrecision);
+typedef void(Processor32)(Float *pDst, ptrdiff_t nDstPitch, const Float *pSrc, ptrdiff_t nSrcPitch, const Float *pRef, ptrdiff_t nRefPitch, int nWidth, int nHeight, int nX, int nY, Float nMinimum, Float nMaximum, int nPrecision);
 
 extern Processor *sad_c;
+extern Processor16 *sad_10_c;
+extern Processor16 *sad_12_c;
+extern Processor16 *sad_14_c;
+extern Processor16 *sad_16_c;
+extern Processor32 *sad_32_c;
 
 class Gradient : public MaskTools::Filter
 {
 
    ProcessorList<Processor> processors;
+   ProcessorList<Processor16> processors16;
+   ProcessorList<Processor32> processors32;
    int nX[3];
    int nY[3];
    int nMinimum;
    int nMaximum;
+   float nMinimum_f;
+   float nMaximum_f;
    int nPrecision;
+
+   int bits_per_pixel;
 
 protected:
 
-    virtual void process(int n, const Plane<Byte> &dst, int nPlane, const Frame<const Byte> frames[3], const Constraint constraints[3]) override
-    {
-        UNUSED(n);
-        processors.best_processor(constraints[nPlane])(dst.data(), dst.pitch(),
-            frames[0].plane(nPlane).data(), frames[0].plane(nPlane).pitch(),
-            frames[1].plane(nPlane).data(), frames[1].plane(nPlane).pitch(),
-            dst.width(), dst.height(), nX[nPlane], nY[nPlane], nMinimum, nMaximum, nPrecision);
-    }
+  virtual void process(int n, const Plane<Byte> &dst, int nPlane, const Frame<const Byte> frames[3], const Constraint constraints[3]) override
+  {
+    UNUSED(n);
+    if (bits_per_pixel == 8)
+      processors.best_processor(constraints[nPlane])(dst.data(), dst.pitch(),
+        frames[0].plane(nPlane).data(), frames[0].plane(nPlane).pitch(),
+        frames[1].plane(nPlane).data(), frames[1].plane(nPlane).pitch(),
+        dst.width(), dst.height(), nX[nPlane], nY[nPlane], nMinimum, nMaximum, nPrecision);
+    else if (bits_per_pixel <= 16)
+      processors16.best_processor(constraints[nPlane])((Word *)dst.data(), dst.pitch(),
+      (Word *)frames[0].plane(nPlane).data(), frames[0].plane(nPlane).pitch(),
+        (Word *)frames[1].plane(nPlane).data(), frames[1].plane(nPlane).pitch(),
+        dst.width(), dst.height(), nX[nPlane], nY[nPlane], nMinimum, nMaximum, nPrecision);
+    else // 32
+      processors32.best_processor(constraints[nPlane])((Float *)dst.data(), dst.pitch(),
+      (Float *)frames[0].plane(nPlane).data(), frames[0].plane(nPlane).pitch(),
+        (Float *)frames[1].plane(nPlane).data(), frames[1].plane(nPlane).pitch(),
+        dst.width(), dst.height(), nX[nPlane], nY[nPlane], nMinimum_f, nMaximum_f, nPrecision);
+  }
 
 public:
    Gradient(const Parameters &parameters) : MaskTools::Filter( parameters, FilterProcessingType::CHILD )
    {
-      if (bit_depths[C] != 8) {
-        error = "only 8 bit clip accepted"; // todo: 10-16bit, float
-        return;
-      }
+      bits_per_pixel = bit_depths[C];
      
       /* add the processors */
-      if ( parameters["distorsion"].toString() == "sad" )
+      if (parameters["distorsion"].toString() == "sad")
       {
-         processors.push_back(Filtering::Processor<Processor>(sad_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0));
-         nMinimum = parameters["min"].is_defined() ? parameters["min"].toInt() : 0;
-         nMaximum = parameters["max"].is_defined() ? parameters["max"].toInt() : 65535;
+        switch (bits_per_pixel) {
+        case 8: processors.push_back(Filtering::Processor<Processor>(sad_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0)); break;
+        case 10: processors16.push_back(Filtering::Processor<Processor16>(sad_10_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0)); break;
+        case 12: processors16.push_back(Filtering::Processor<Processor16>(sad_12_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0)); break;
+        case 14: processors16.push_back(Filtering::Processor<Processor16>(sad_14_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0)); break;
+        case 16: processors16.push_back(Filtering::Processor<Processor16>(sad_16_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0)); break;
+        case 32: processors32.push_back(Filtering::Processor<Processor32>(sad_32_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0)); break;
+        }
+        if (bits_per_pixel <= 16) {
+          nMinimum = parameters["min"].is_defined() ? parameters["min"].toInt() : 0;
+          nMaximum = parameters["max"].is_defined() ? parameters["max"].toInt() : 16 * 16 * (1 << bits_per_pixel); // max sad of 16x16 block
+        }
+        else {
+          nMinimum_f = parameters["min"].is_defined() ? (Float)parameters["min"].toFloat() : 0.0f;
+          nMaximum_f = parameters["max"].is_defined() ? (Float)parameters["max"].toFloat() : 16 * 16 * 1.0f; // max sad of block size 16x16 
+        }
       }
       else 
       {
@@ -73,8 +107,8 @@ public:
       signature.add(Parameter(String("sad"), "distorsion"));
       signature.add(Parameter(0, "size_x"));
       signature.add(Parameter(0, "size_y"));
-      signature.add(Parameter(0, "min"));
-      signature.add(Parameter(65535, "max"));
+      signature.add(Parameter(0.0f, "min"));
+      signature.add(Parameter(65535.0f, "max")); // bit depth adaptive 
       signature.add(Parameter(1, "precision"));
 
       return add_defaults( signature );
