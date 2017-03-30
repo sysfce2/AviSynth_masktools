745a8e82c9e74a80612d3517548d8480299038cb
Pinterf <pinterfer@gmail.com>
2017-03-30 16:00:19 +0200

    lut compute: distinct 1-4 param calls for float, implicite bitdepths for ints + fix predef.const array size

--- common/parser/symbol.h
+++ common/parser/symbol.h
@@ -199,15 +199,15 @@ class Context {
    int sbitdepth; // source bit depth of values to scale
    double sbitdepth_f; // source bit depth of values to scale, avoid conversions
 
-   // predefined bit-depth dependent constant 8..16 -> 0..7
+   // predefined bit-depth dependent constant 8..16 -> 0..8
    // calculated in advance for realtime speed gain
-   double a_range_half[8];
-   double a_range_max[8];
-   double a_range_size[8];
-   double a_ymin[8];
-   double a_ymax[8];
-   double a_cmin[8];
-   double a_cmax[8];
+   double a_range_half[9];
+   double a_range_max[9];
+   double a_range_size[9];
+   double a_ymin[9];
+   double a_ymax[9];
+   double a_cmin[9];
+   double a_cmax[9];
    double range_half_f; // or 0.0 for float chroma in the future?
    double range_max_f;
    double range_size_f;
@@ -239,19 +239,19 @@ public:
    bool check();
    String infix();
 
-   double compute_1(double x, int bitdepth = 8);
-   double compute_2(double x, double y, int bitdepth = 8);
-   double compute_3(double x, double y, double z, int bitdepth = 8);
-   double compute_4(double x, double y, double z, double a, int bitdepth = 8);
+   double compute_1(double x, int bitdepth);
+   double compute_2(double x, double y, int bitdepth);
+   double compute_3(double x, double y, double z, int bitdepth);
+   double compute_4(double x, double y, double z, double a, int bitdepth);
    // v2.2.1: variable a
-   double compute(double x, double y = -1.0f, double z = -1.0f, double a = -1.0f, int bitdepth = 8);
+   double compute(double x, double y = -1.0, double z = -1.0, double a = -1.0, int bitdepth = 8);
    
-   MT_FORCEINLINE Byte compute_byte_x(int _x, int _bitdepth = 8) { return clip<Byte, double>(compute_1(_x, _bitdepth)); } // byte: default 8 bit
-   MT_FORCEINLINE Byte compute_byte_xy(int _x, int _y, int _bitdepth = 8) { return clip<Byte, double>(compute_2(_x, _y, _bitdepth)); } // byte: default 8 bit
-   MT_FORCEINLINE Byte compute_byte_xyz(int _x, int _y, int _z, int _bitdepth = 8) { return clip<Byte, double>(compute_3(_x, _y, _z, _bitdepth)); } // byte: default 8 bit
-   MT_FORCEINLINE Byte compute_byte_xyza(int _x, int _y, int _z, int _a, int _bitdepth = 8) { return clip<Byte, double>( compute_4(_x, _y, _z, _a, _bitdepth) ); } // byte: default 8 bit
+   MT_FORCEINLINE Byte compute_byte_x(int _x) { return clip<Byte, double>(compute_1(_x, 8)); } // byte: default 8 bit
+   MT_FORCEINLINE Byte compute_byte_xy(int _x, int _y) { return clip<Byte, double>(compute_2(_x, _y, 8)); } // byte: default 8 bit
+   MT_FORCEINLINE Byte compute_byte_xyz(int _x, int _y, int _z) { return clip<Byte, double>(compute_3(_x, _y, _z, 8)); } // byte: default 8 bit
+   MT_FORCEINLINE Byte compute_byte_xyza(int _x, int _y, int _z, int _a) { return clip<Byte, double>( compute_4(_x, _y, _z, _a, 8) ); } // byte: default 8 bit
    
-   Byte compute_byte_xy_dblinput(double _x, double _y, int _bitdepth = 8) { return clip<Byte, double>(compute_2(_x, _y, _bitdepth)); } // byte: default 8 bit
+   Byte compute_byte_xy_dblinput(double _x, double _y) { return clip<Byte, double>(compute_2(_x, _y, 8)); } // byte: default 8 bit
 
    template<int bits_per_pixel>
    MT_FORCEINLINE Word compute_word_x(int _x) {
@@ -300,7 +300,7 @@ public:
      return min(clip<Word, double>(compute_2(_x, _y, _bitdepth)), (Word)((1 << _bitdepth) - 1));
    } // clamp valid range for 10-14 bits
 
-   MT_FORCEINLINE Float compute_float(double _x, double _y = -1.0f, double _z = -1.0f, double _a = -1.0f, int _bitdepth = 32)
+   MT_FORCEINLINE Float compute_float(double _x, double _y = -1.0, double _z = -1.0, double _a = -1.0, int _bitdepth = 32)
    { 
      // todo: make it a bit faster if less real parameter is provided
      float result;
@@ -319,6 +319,89 @@ public:
        return result;
      }
    }
+
+   MT_FORCEINLINE Float compute_float_x(double _x)
+   {
+     float result;
+     if (float_autoscale_bitdepth == 0)
+       return (float)(compute_1(_x, 32));
+     else {
+       // input bitdepth = 32 and autoScale mode is on
+       if (float_autoscale_bitdepth == 32) {
+         // scaling target is 32 bit, no scaling needed, but clamping is still on
+         return max(min((float)((compute_1(_x, 32))), 1.0f), 0.0f);
+       }
+
+       // we pass the fake bitdepth (8..16) float_autoscale_bitdepth, scale up input, scale down result
+       result = (float)(float_input_invscalefactor*(compute_1(float_input_scalefactor*_x, float_autoscale_bitdepth)));
+       result = max(min(result, 1.0f), 0.0f);
+       return result;
+     }
+   }
+
+   MT_FORCEINLINE Float compute_float_xy(double _x, double _y)
+   {
+     float result;
+     if (float_autoscale_bitdepth == 0)
+       return (float)(compute_2(_x, _y, 32));
+     else {
+       // input bitdepth = 32 and autoScale mode is on
+       if (float_autoscale_bitdepth == 32) {
+         // scaling target is 32 bit, no scaling needed, but clamping is still on
+         return max(min((float)((compute(_x, _y, 32))), 1.0f), 0.0f);
+       }
+
+       // we pass the fake bitdepth (8..16) float_autoscale_bitdepth, scale up input, scale down result
+       result = (float)(float_input_invscalefactor*(compute_2(float_input_scalefactor*_x, float_input_scalefactor*_y, float_autoscale_bitdepth)));
+       result = max(min(result, 1.0f), 0.0f);
+       return result;
+     }
+   }
+
+   template<int _bitdepth>
+   MT_FORCEINLINE Float compute_float_xy_intinput(int _x, int _y)
+   {
+     return (float)(compute_2(_x, _y, _bitdepth));
+   }
+
+   MT_FORCEINLINE Float compute_float_xyz(double _x, double _y, double _z)
+   {
+     float result;
+     if (float_autoscale_bitdepth == 0)
+       return (float)(compute_3(_x, _y, _z, 32));
+     else {
+       // input bitdepth = 32 and autoScale mode is on
+       if (float_autoscale_bitdepth == 32) {
+         // scaling target is 32 bit, no scaling needed, but clamping is still on
+         return max(min((float)((compute_3(_x, _y, _z, 32))), 1.0f), 0.0f);
+       }
+
+       // we pass the fake bitdepth (8..16) float_autoscale_bitdepth, scale up input, scale down result
+       result = (float)(float_input_invscalefactor*(compute_3(float_input_scalefactor*_x, float_input_scalefactor*_y, float_input_scalefactor*_z, float_autoscale_bitdepth)));
+       result = max(min(result, 1.0f), 0.0f);
+       return result;
+     }
+   }
+
+   MT_FORCEINLINE Float compute_float_xyza(double _x, double _y, double _z, double _a)
+   {
+     float result;
+     if (float_autoscale_bitdepth == 0)
+       return (float)(compute_4(_x, _y, _z, _a, 32));
+     else {
+       // input bitdepth = 32 and autoScale mode is on
+       if (float_autoscale_bitdepth == 32) {
+         // scaling target is 32 bit, no scaling needed, but clamping is still on
+         return max(min((float)((compute_4(_x, _y, _z, _a, 32))), 1.0f), 0.0f);
+       }
+
+       // we pass the fake bitdepth (8..16) float_autoscale_bitdepth, scale up input, scale down result
+       result = (float)(float_input_invscalefactor*(compute_4(float_input_scalefactor*_x, float_input_scalefactor*_y, float_input_scalefactor*_z, float_input_scalefactor*_a, float_autoscale_bitdepth)));
+       result = max(min(result, 1.0f), 0.0f);
+       return result;
+     }
+   }
+
 };
 
 } } // namespace Parser, Filtering
